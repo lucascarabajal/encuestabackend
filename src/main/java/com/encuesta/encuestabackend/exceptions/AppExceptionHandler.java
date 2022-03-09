d78bb605a17b4c655258b0ed71ef2f99550e6e5d
Lucas Carabajal <lucascar007@gmail.com>
2022-03-09 14:42:48 -0300

    Adding Exception for users

---
+++ src/main/java/com/encuesta/encuestabackend/exceptions/AppExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.encuesta.encuestabackend.exceptions;
+
+import java.io.ObjectInputStream.GetField;
+import java.lang.reflect.Method;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.encuesta.encuestabackend.models.responses.ValidationErrors;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class AppExceptionHandler{
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<Object> handleValidationErrorException(MethodArgumentNotValidException ex, WebRequest webRequest){
+        
+        Map<String,String> errors = new HashMap<>();
+
+        for (ObjectError error : ex.getBindingResult().getAllErrors()){
+            String fielName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fielName,errorMessage);
+        }
+        
+        ValidationErrors validationErrors = new ValidationErrors(errors, new Date());
+        
+        return new ResponseEntity<>(validationErrors, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+}
