952bc1cb1b3bb3e044db121df9212e0e47d1aee6
Lucas Carabajal <lucascar007@gmail.com>
2022-03-22 10:08:02 -0300

    LoginTest

--- src/test/java/com/encuesta/encuestabackend/LoginTests.java
+++ src/test/java/com/encuesta/encuestabackend/LoginTests.java
@@ -1,14 +1,25 @@
 package com.encuesta.encuestabackend;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Map;
 
 import com.encuesta.encuestabackend.models.request.UserLoginRequestModel;
+import com.encuesta.encuestabackend.models.request.UserRegisterRequestModel;
+import com.encuesta.encuestabackend.repository.UserRepository;
+import com.encuesta.encuestabackend.services.UserService;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
@@ -23,13 +34,77 @@ public class LoginTests {
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @BeforeEach
+    public void cleanup(){
+        userRepository.deleteAll();
+    }
+
     @Test
     public void postLogin_sinCredenciales_retornaForbidden(){
         ResponseEntity<Object> response = login(null, Object.class);
         assertEquals(response.getStatusCode(), HttpStatus.FORBIDDEN);
     }
 
+    @Test
+    public void postLogin_conCredencialesIncorrectas_retornaUnathorized(){
+
+        UserRegisterRequestModel user = TestUtil.createValidUser();
+        userService.createUser(user);
+
+        UserLoginRequestModel model = new UserLoginRequestModel();
+        model.setEmail("asd@gmail.com");
+        model.setPassword("password");
+
+        ResponseEntity<Object> response = login(model, Object.class);
+        assertEquals(response.getStatusCode(), HttpStatus.FORBIDDEN);
+    }
+
+    @Test
+    public void postLogin_conCredencialesCorrectas_retornaOk(){
+
+        UserRegisterRequestModel user = TestUtil.createValidUser();
+        userService.createUser(user);
+        
+        UserLoginRequestModel model = new UserLoginRequestModel();
+        model.setEmail(user.getEmail());
+        model.setPassword(user.getPassword());
+
+        ResponseEntity<Object> response = login(model, Object.class);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+    }
+
+    @Test
+    public void postLogin_conCredencialesCorrectas_retornaAuthToken(){
+
+        UserRegisterRequestModel user = TestUtil.createValidUser();
+        userService.createUser(user);
+        
+        UserLoginRequestModel model = new UserLoginRequestModel();
+        model.setEmail(user.getEmail());
+        model.setPassword(user.getPassword());
+
+        ResponseEntity<Map<String,String>> response = 
+        login(model, new ParameterizedTypeReference<Map<String,String>>(){});
+
+        Map<String,String> body = response.getBody();
+
+        String token = body.get("token");
+
+        assertTrue(token.contains("Bearer"));
+    }
+
     public <T> ResponseEntity<T> login(UserLoginRequestModel data,Class<T> responseType){
         return testRestTemplate.postForEntity(API_LOGIN_URL, data, responseType);
+    }
+
+    public <T> ResponseEntity<T> login(UserLoginRequestModel data,ParameterizedTypeReference<T> responseType){
+        HttpEntity<UserLoginRequestModel> entity = new HttpEntity<UserLoginRequestModel>(data,new HttpHeaders());
+        return testRestTemplate.exchange(API_LOGIN_URL, HttpMethod.POST, entity, responseType);
     }
 }
---
+++ src/test/java/com/encuesta/encuestabackend/TestUtil.java
@@ -0,0 +1,29 @@
+package com.encuesta.encuestabackend;
+
+import java.util.Random;
+
+import com.encuesta.encuestabackend.models.request.UserRegisterRequestModel;
+
+public class TestUtil {
+    public static UserRegisterRequestModel createValidUser(){
+        UserRegisterRequestModel user = new UserRegisterRequestModel();
+        user.setEmail(generateRamdomString(8)+"@gmail.com");
+        user.setName(generateRamdomString(8));
+        user.setPassword(generateRamdomString(8));
+        return user;
+    }
+
+    public static String generateRamdomString(int len){
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWQYZabcdefghijklmnopqrstuvwxyz";
+
+        Random rnd = new Random();
+
+        StringBuilder sb = new StringBuilder(len);
+
+        for(int i =0; i<sb.length(); i++){
+            sb.append(chars.charAt(rnd.nextInt(chars.length())));
+        }
+
+        return sb.toString();
+    }
+}
